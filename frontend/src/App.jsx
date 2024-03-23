1956bcb4cf3ca6975a2926a758bac1cb400c4306
atian155 <atianthony@outlook.com>
2024-03-23 14:58:51 +0200

    Changed auth page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,33 +10,41 @@ import "./styles/inputs.css";
 import logo from "./assets/images/logo.svg";
 
 // React Router
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 // Pages
 import Authentication from "./pages/Authentication";
 
 const App = () => {
   return (
-    <main className="container">
-      <header className="site-header">
-        <img src={logo} alt="LinkedIn" />
-        <nav className="site-nav">
-          <a href="/">Home</a>
-          <a href="/">About</a>
-          <a href="/">Company</a>
-          <button className="button button-transparent">Join now</button>
-          <button className="button button-outlined button-outlined-primary">
-            Sign in
-          </button>
-        </nav>
-      </header>
-      <BrowserRouter>
+    <BrowserRouter>
+      <main className="container">
+        <header className="site-header">
+          <img src={logo} alt="LinkedIn" />
+          <nav className="site-nav">
+            <a href="/">Home</a>
+            <a href="/">About</a>
+            <a href={process.env.REACT_APP_API_URL}>Company</a>
+            <Link
+              className="button button-transparent"
+              to="auth?authType=signup"
+            >
+              Join now
+            </Link>
+            <Link
+              className="button button-outlined button-outlined-primary"
+              to="auth"
+            >
+              Sign in
+            </Link>
+          </nav>
+        </header>
         <Routes>
           <Route path="/" element={<>Hello World!</>} />
           <Route path="/auth" element={<Authentication />} />
         </Routes>
-      </BrowserRouter>
-    </main>
+      </main>
+    </BrowserRouter>
   );
 };
 
--- frontend/src/pages/Authentication/index.jsx
+++ frontend/src/pages/Authentication/index.jsx
@@ -1,5 +1,6 @@
 // React stuff
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 // Assets
 import authImg from "../../assets/images/auth_image.svg";
@@ -12,14 +13,33 @@ import LoginForm from "./components/LoginForm";
 import SignupForm from "./components/SignupForm";
 
 const Authentication = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [isLogin, setIsLogin] = useState(true);
+
+  useEffect(() => {
+    const authType = searchParams.get("authType");
+    if (authType === "signup") {
+      setIsLogin(false);
+    } else {
+      setIsLogin(true);
+    }
+  }, [searchParams]);
+
   return (
     <div className="auth-container">
-      <div className="auth-form">
+      <div className="auth-intro">
         <h1 className="display-1 font-thin">
           Welcome to your professional community
         </h1>
-        {isLogin ? <LoginForm /> : <SignupForm />}
+        <div className="auth-form">
+          {isLogin ? <LoginForm /> : <SignupForm />}
+          <button
+            className="button button-outlined"
+            onClick={() => setIsLogin(!isLogin)}
+          >
+            {isLogin ? "New to LinkedIn? Join now" : "Already have an account?"}
+          </button>
+        </div>
       </div>
       <div className="auth-img">
         <img src={authImg} alt="" />
